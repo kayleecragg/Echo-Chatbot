e367b938f16112e22c7f7b14730cf5c19e20b24b
Kaylee Cragg <tobajuice@gmail.com>
2022-02-05 20:59:01 +1000

    Add files via upload

---
+++ echo-chatbot.py
@@ -0,0 +1,6 @@
+print("""Welcome to Kaylee's first chatbot!""")
+print("""This chatbot will repeat what you type to it.""")
+
+stuff_to_echo = input("""Please enter something for the program to repeat. """)
+
+print("You said: " + stuff_to_echo)
